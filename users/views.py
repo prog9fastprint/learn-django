a8adde04028ecfbeedce594ade9851687d8f5ba1
prog9fastprint <prog9.fastprintsby@gmail.com>
2024-04-05 15:54:37 +0700

    feat: Authentication

--- users/views.py
+++ users/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, 'Account created for {}'.format(username))
-            return redirect('blog-home')
+            messages.success(request, 'Account has been created {}, Login to start your journey!'.format(username))
+            return redirect('users-login')
     else :
-        form = UserCreationForm
+        form = UserRegistrationForm
     return render(request, 'users/register.html', {'form':form})
+
+def signout(request):
+    logout(request)
+    return redirect('users-login')
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+    
